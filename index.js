8434f0a86152609a08a104bbd35b5a9ee9f1d749
Eric Chan <kakchan@gmail.com>
2018-12-11 16:14:11 +1100

    add jest and update getwithFilter function

--- index.js
+++ index.js
@@ -1,115 +1,61 @@
-/* ================================================================ *
-    ajaxzip3.js ---- AjaxZip3 郵便番号→住所変換ライブラリ
-
-    Copyright (c) 2015 MIZUNO Hiroki
-    http://github.com/mzp/japan-postal-code
-
-    Copyright (c) 2008-2015 Ninkigumi Co.,Ltd.
-    http://ajaxzip3.github.io/
-
-    Copyright (c) 2006-2007 Kawasaki Yusuke <u-suke [at] kawa.net>
-    http://www.kawa.net/works/ajax/AjaxZip2/AjaxZip2.html
-
-    Permission is hereby granted, free of charge, to any person
-    obtaining a copy of this software and associated documentation
-    files (the "Software"), to deal in the Software without
-    restriction, including without limitation the rights to use,
-    copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following
-    conditions:
-
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
-* ================================================================ */
-
-var jsonp = require('jsonp');
-
-var JSONDATA = '/zipdata/zip-'; // Path or URL
-var CALLBACK_NAME = 'zipdata';
-var CACHE = [];
+import request from 'request';
+import { take } from 'ramda';
 
 
-var PREFMAP = [
-    null,       '北海道',   '青森県',   '岩手県',   '宮城県',
-    '秋田県',   '山形県',   '福島県',   '茨城県',   '栃木県',
-    '群馬県',   '埼玉県',   '千葉県',   '東京都',   '神奈川県',
-    '新潟県',   '富山県',   '石川県',   '福井県',   '山梨県',
-    '長野県',   '岐阜県',   '静岡県',   '愛知県',   '三重県',
-    '滋賀県',   '京都府',   '大阪府',   '兵庫県',   '奈良県',
-    '和歌山県', '鳥取県',   '島根県',   '岡山県',   '広島県',
-    '山口県',   '徳島県',   '香川県',   '愛媛県',   '高知県',
-    '福岡県',   '佐賀県',   '長崎県',   '熊本県',   '大分県',
-    '宮崎県',   '鹿児島県', '沖縄県'
+let CALLBACK_NAME = 'zipdata';
+const CACHE = [];
+
+
+const PREFMAP = [
+  null, '北海道', '青森県', '岩手県', '宮城県',
+  '秋田県', '山形県', '福島県', '茨城県', '栃木県',
+  '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
+  '新潟県', '富山県', '石川県', '福井県', '山梨県',
+  '長野県', '岐阜県', '静岡県', '愛知県', '三重県',
+  '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県',
+  '和歌山県', '鳥取県', '島根県', '岡山県', '広島県',
+  '山口県', '徳島県', '香川県', '愛媛県', '高知県',
+  '福岡県', '佐賀県', '長崎県', '熊本県', '大分県',
+  '宮崎県', '鹿児島県', '沖縄県'
 ];
 
-var PREFMAP_EN = [
-    null,        'Hokkaido',  'Aomori',    'Iwate',    'Miyagi',
-    'Akita',     'Yamagata',  'Fukushima', 'Ibaraki',  'Tochigi',
-    'Gumma',     'Saitama',   'Chiba',     'Tokyo',    'Kanagawa',
-    'Niigata',   'Toyama',    'Ishikawa',  'Fukui',    'Yamanashi',
-    'Nagano',    'Gifu',      'Shizuoka',  'Aichi',    'Mie',
-    'Shiga',     'Kyoto',     'Osaka',     'Hyogo',    'Nara',
-    'Wakayama',  'Tottori',   'Shimane',   'Okayama',  'Hiroshima',
-    'Yamaguchi', 'Tokushima', 'Kagawa',    'Ehime',    'Kochi',
-    'Fukuoka',   'Saga',      'Nagasaki',  'Kumamoto', 'Oita',
-    'Miyazaki',  'Kagoshima', 'Okinawa'
+const PREFMAP_EN = [
+  null, 'Hokkaido', 'Aomori', 'Iwate', 'Miyagi',
+  'Akita', 'Yamagata', 'Fukushima', 'Ibaraki', 'Tochigi',
+  'Gumma', 'Saitama', 'Chiba', 'Tokyo', 'Kanagawa',
+  'Niigata', 'Toyama', 'Ishikawa', 'Fukui', 'Yamanashi',
+  'Nagano', 'Gifu', 'Shizuoka', 'Aichi', 'Mie',
+  'Shiga', 'Kyoto', 'Osaka', 'Hyogo', 'Nara',
+  'Wakayama', 'Tottori', 'Shimane', 'Okayama', 'Hiroshima',
+  'Yamaguchi', 'Tokushima', 'Kagawa', 'Ehime', 'Kochi',
+  'Fukuoka', 'Saga', 'Nagasaki', 'Kumamoto', 'Oita',
+  'Miyazaki', 'Kagoshima', 'Okinawa'
 ];
-
-
-exports.setJsonDataUrl = function(url) {
-  JSONDATA = url;
-};
 
 exports.setCallbackName = function(name) {
   CALLBACK_NAME = name;
 };
 
-exports.get = function(_postalcode, callback) {
-  getWithFilter(_postalcode, callback, function(addresses) {
-    if (addresses) return addresses[0];
-    return null;
-  });
-};
+exports.get = _postalcode => getWithFilter(_postalcode);
 
-exports.getMulti = function(_postalcode, callback) {
-  getWithFilter(_postalcode, callback, function(addresses) {
-    if (addresses) return addresses;
-    return [];
-  });
-};
-
-var cache = function(postalcode3, records) {
+const cache = function (postalcode3, records) {
   if (records) {
     CACHE[postalcode3] = records;
   }
   return CACHE[postalcode3];
 };
 
-var jsonpUrl = function(postalcode3) {
-  return JSONDATA + postalcode3 + '.js';
+const postCodeFileUrl = fileName => `https://static-assets.handcarry.co.jp/data/japan_postcode/zip-${fileName}.js`;
+
+const normalizePostalcode = postCode => {
+  if (!postCode) { return null; }
+  return postCode.replace(/[^0-9]/, '');
 };
 
-var normalizePostalcode = function(postalcode) {
-  if (!postalcode) return null;
-  var normalized = postalcode.replace(/[^0-9]/, ''); // extract number only
-  if (normalized.length < 7) return null;
-  return normalized;
-};
-
-var lookupAddresses = function(postalcode, _records) {
+const lookupAddresses = function (postalcode, _records) {
   var rows = _records[postalcode];
   // Opera バグ対策：0x00800000 を超える添字は +0xff000000 されてしまう
-  var opera = (postalcode-0+0xff000000)+"";
+  var opera = (postalcode - 0 + 0xff000000) + "";
   if (!rows && _records[opera]) rows = _records[opera];
   if (!rows) return null;
 
@@ -121,24 +67,24 @@ var lookupAddresses = function(postalcode, _records) {
   return addresses;
 };
 
-var parse = function(row) {
+const parse = function(row) {
   if (!row) return null;
-  var prefectureId = row[0];
+  let prefectureId = row[0];
   if (!prefectureId) return null;
-  var prefectureJa = PREFMAP[prefectureId];
+  let prefectureJa = PREFMAP[prefectureId];
   if (!prefectureJa) return null;
-  var prefectureEn = PREFMAP_EN[prefectureId];
+  let prefectureEn = PREFMAP_EN[prefectureId];
   if (!prefectureEn) return null;
 
-  var cityJa   = row[1] || '';
-  var areaJa   = row[2] || '';
-  var streetJa = row[3] || '';
-  var cityEn   = row[4] || '';
-  var areaEn   = row[5] || '';
-  var streetEn = row[6] || '';
+  const cityJa = row[1] || '';
+  const areaJa = row[2] || '';
+  const streetJa = row[3] || '';
+  const cityEn = row[4] || '';
+  const areaEn = row[5] || '';
+  const streetEn = row[6] || '';
 
-  var addressJa = prefectureJa + cityJa + areaJa + streetJa;
-  var addressEn = prefectureEn + ', Japan';
+  const addressJa = prefectureJa + cityJa + areaJa + streetJa;
+  let addressEn = prefectureEn + ', Japan';
   if (cityEn)   addressEn = cityEn   + ', ' + addressEn;
   if (areaEn)   addressEn = areaEn   + ', ' + addressEn;
   if (streetEn) addressEn = streetEn + ', ' + addressEn;
@@ -158,28 +104,31 @@ var parse = function(row) {
   };
 };
 
-var getWithFilter = function (_postalcode, callback, filter) {
-  var postalcode = normalizePostalcode(_postalcode);
-  if (!postalcode) {
-    callback(filter(null));
-    return;
-  }
-  var postalcode3 = postalcode.substr(0, 3);
+const MAX_RETURN_ITEMS = 10;
 
-  var records = cache(postalcode3);
-  if (records) {
-    var addresses = lookupAddresses(postalcode, records);
-    callback(filter(addresses));
-    return;
+export const getWithFilter = _postCode => {
+  const postCode = normalizePostalcode(_postCode);
+  if (!postCode || postCode.length < 3) {
+    return Promise.resolve();
   }
-
-  jsonp(jsonpUrl(postalcode3), { name: CALLBACK_NAME }, function(error, records) {
-    if (error) {
-      callback(filter(null));
-    } else {
-      cache(postalcode3, records);
-      var addresses = lookupAddresses(postalcode, records);
-      callback(filter(addresses));
-    }
-  });
+  const fileName = postCode.substr(0, 3);
+  return (new Promise((resolve, reject) => {
+    request(postCodeFileUrl(fileName), function(err, resp) {
+      if (err) {
+        return reject(err);
+      }
+      const jsonString = resp.body.trim().replace(/(zipdata\(|\)$)/g, '');
+      const json = JSON.parse(jsonString);
+      let returnArray = [];
+      for (let name in json) {
+        if ((new RegExp(postCode)).test(name)) {
+          returnArray = returnArray.concat(json[name]);
+          if (returnArray.length >= MAX_RETURN_ITEMS) {
+            break;
+          }
+        }
+      }
+      resolve(take(MAX_RETURN_ITEMS, returnArray).map(item => parse(item)));
+    })
+  }));
 };
---
+++ index.spec.js
@@ -0,0 +1,187 @@
+import {getWithFilter, transformDataArrayToAddress} from "./index";
+
+describe('Japan Post code', () => {
+  describe('getWithFilter()', () => {
+    describe('undefined', () => {
+      let results;
+      beforeEach(() =>
+        getWithFilter()
+          .then(res => results = res)
+      );
+
+      it('should return undefined', () => {
+        expect(results).toBeUndefined();
+      });
+    });
+
+    describe('postcode length is less than 3', () => {
+      let results;
+      beforeEach(() =>
+        getWithFilter("10")
+          .then(res => results = res)
+      );
+
+      it('should return undefined', () => {
+        expect(results).toBeUndefined();
+      });
+    });
+
+    describe('postcode length is 3 - partial match', () => {
+      let results;
+      beforeEach(() =>
+        getWithFilter("001")
+          .then(res => results = res)
+      );
+
+      it('should return max 10 items', () => {
+        expect(results).toEqual([{
+            "address": "北海道札幌市北区",
+            "addressEn": "Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "",
+            "areaEn": "",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十条西",
+            "addressEn": "Kita10-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十条西",
+            "areaEn": "Kita10-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十一条西",
+            "addressEn": "Kita11-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十一条西",
+            "areaEn": "Kita11-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十二条西",
+            "addressEn": "Kita12-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十二条西",
+            "areaEn": "Kita12-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十三条西",
+            "addressEn": "Kita13-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十三条西",
+            "areaEn": "Kita13-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十四条西",
+            "addressEn": "Kita14-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十四条西",
+            "areaEn": "Kita14-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十五条西",
+            "addressEn": "Kita15-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十五条西",
+            "areaEn": "Kita15-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十六条西",
+            "addressEn": "Kita16-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十六条西",
+            "areaEn": "Kita16-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十七条西",
+            "addressEn": "Kita17-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十七条西",
+            "areaEn": "Kita17-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }, {
+            "address": "北海道札幌市北区北十八条西",
+            "addressEn": "Kita18-jonishi, Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "北十八条西",
+            "areaEn": "Kita18-jonishi",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }]
+        );
+      });
+    });
+
+    describe('postcode length is 7 with dash - complete match', () => {
+      let results;
+      beforeEach(() =>
+        getWithFilter("001-0000")
+          .then(res => results = res)
+      );
+
+      it('should return 1 item', () => {
+        expect(results).toEqual([{
+            "address": "北海道札幌市北区",
+            "addressEn": "Kita Ward, Sapporo, Hokkaido, Japan",
+            "area": "",
+            "areaEn": "",
+            "city": "札幌市北区",
+            "cityEn": "Kita Ward, Sapporo",
+            "prefecture": "北海道",
+            "prefectureEn": "Hokkaido",
+            "prefectureId": 1,
+            "street": "",
+            "streetEn": ""
+          }]
+        );
+      });
+    });
+  });
+});
